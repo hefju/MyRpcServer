00c619284263f9b078f9605c6c5072196a536a5c
hefju <hefju@live.cn>
2015-04-15 23:24:18 +0800

    rpc server可以运行了

--- Main.go
+++ Main.go
@@ -1,5 +1,50 @@
 package main
-import "fmt"
+import (
+//    "github.com/gorilla/rpc"
+//    "github.com/gorilla/rpc/json"
+//    "net/http"
+//    "fmt"
+    "log"
+    "net"
+    "net/rpc"
+    "net/rpc/jsonrpc"
+)
+
+type Args struct{
+    Who string
+}
+type Reply struct {
+    Message string
+}
+type Hello struct {
+}
+func (h *Hello)Say(args *Args, reply *Reply)error{
+    reply.Message="hello,"+args.Who+"!"
+    return  nil
+}
+
 func main(){
-    fmt.Println("end")
+
+    hello:=new(Hello)
+    s:=rpc.NewServer()
+    s.Register(hello)
+    s.HandleHTTP(rpc.DefaultRPCPath,rpc.DefaultDebugPath)
+    l,e:=net.Listen("tcp",":8081")
+    if e!=nil{
+        log.Fatal("listen:",e)
+    }
+    for{
+        if conn,err:=l.Accept();err!=nil{
+            log.Fatal("accept:",err.Error())
+        }else{
+            log.Printf("new connect")
+            go s.ServeCodec(jsonrpc.NewServerCodec(conn))
+        }
+    }
+//    s:=rpc.NewServer()
+//    s.RegisterCodec(json.NewCodec(),"application/json")
+//    s.RegisterService(new(Hello),"")
+//    http.Handle("/rpc",s)
+//    http.ListenAndServe(":8081",nil)
+//    fmt.Println("end")
 }
